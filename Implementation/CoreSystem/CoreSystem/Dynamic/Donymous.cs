11cec145377077d2a333805ab1ae47032294fbe1
Faraz Masood Khan <mk.faraz@gmail.com>
2012-09-11 17:25:46 +0500

    Donymous class to make anonymous object dynamic which can be used in Razor templates

---
+++ Implementation/CoreSystem/CoreSystem/Dynamic/Donymous.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Dynamic;
+
+namespace CoreSystem.Dynamic
+{
+    /// <summary>
+    /// Expand any object to dynamic object
+    /// </summary>
+    public class Donymous : DynamicObject
+    {
+        private object obj;
+        private Type objType;
+        private Dictionary<string, object> memberValues = new Dictionary<string, object>();
+
+        public Donymous(object obj)
+        {
+            this.obj = obj;
+            this.objType = obj.GetType();
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            if (!this.memberValues.TryGetValue(binder.Name, out result))
+            {
+                var property = this.objType.GetProperty(binder.Name);
+                if (property != null)
+                {
+                    result = property.GetValue(this.obj, null);
+                    this.memberValues[binder.Name] = result;
+                    return true;
+                }
+            }
+            else
+                return true;
+
+            return base.TryGetMember(binder, out result);
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            this.memberValues[binder.Name] = value;
+            return true;
+        }
+    }
+}
---
+++ Implementation/CoreSystem/CoreSystem/RefTypeExtension/ObjectExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoreSystem.Dynamic;
+
+namespace CoreSystem.RefTypeExtension
+{
+    public static class ObjectExtension
+    {
+        public static Donymous ToDonymous(this object obj)
+        {
+            return obj == null ? null : new Donymous(obj);
+        }
+    }
+}
