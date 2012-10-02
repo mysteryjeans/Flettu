27204076334cf6914571aed9f8ee634a159d8cfd
Faraz Masood Khan <faraz@fanaticlab.com>
2012-10-02 21:53:27 +0500

    ToFriendlyDate extension function added for DateTime conversion to user friendly dates e.g "2 mins ago"

--- Implementation/CoreSystem/CoreSystem/ValueTypeExtension/DataTimeExtension.cs
+++ Implementation/CoreSystem/CoreSystem/ValueTypeExtension/DataTimeExtension.cs
@@ -5,13 +5,59 @@ using System.Text;
 
 namespace CoreSystem.ValueTypeExtension
 {
+    /// <summary>
+    /// Helper functions for DateTime
+    /// </summary>
     public static class DataTimeExtension
     {
         private static readonly DateTime UnixRefereceDataTime = (new DateTime(1970, 1, 1, 0, 0, 0, 0)).ToUniversalTime();
 
+        /// <summary>
+        /// Time passed since 1st, January 1970
+        /// </summary>
+        /// <param name="dateTime">Value from to which calculate unix reference</param>
+        /// <returns>Total seconds password since 1st, January 1970</returns>
         public static long GetUnixTime(this DateTime dateTime)
         {
             return (long)(dateTime.ToUniversalTime() - UnixRefereceDataTime).TotalSeconds;
+        }
+
+        /// <summary>
+        /// Time passed since specified value in user friendly string
+        /// </summary>
+        /// <param name="dateTime">Value to convert in user friendly string</param>
+        /// <returns>User friendly datetime string</returns>
+        public static string ToFriendlyDate(this DateTime dateTime)
+        {
+            return dateTime.ToFriendlyDate(DateTime.Now);
+        }
+
+        /// <summary>
+        /// Time passed since specified value in user friendly string
+        /// </summary>
+        /// <param name="dateTime">Value to convert in user friendly string</param>
+        /// <param name="currentTime">Value to take reference as current time when converting to user friendly string</param>
+        /// <returns>User friendly datetime string</returns>
+        public static string ToFriendlyDate(this DateTime dateTime, DateTime currentTime)
+        {
+            var timespan = currentTime - dateTime;
+
+            if (timespan.Days > 365)
+                return string.Format("{0} year{1} ago", timespan.Days / 365, (timespan.Days / 365) > 1 ? "s" : "");
+
+            if (timespan.Days > 30)
+                return string.Format("{0} month{1} ago", timespan.Days / 30, (timespan.Days / 30) > 1 ? "s" : "");
+
+            if (timespan.Days > 0)
+                return string.Format("{0} day{1} ago", timespan.Days, timespan.Days > 1 ? "s" : "");
+
+            if (timespan.Hours > 0)
+                return string.Format("{0} hour{1} ago", timespan.Hours, timespan.Hours > 1 ? "s" : "");
+
+            if (timespan.Minutes > 0)
+                return string.Format("{0} minute{1} ago", timespan.Minutes, timespan.Minutes > 1 ? "s" : "");
+
+            return "A moment ago";
         }
     }
 }
