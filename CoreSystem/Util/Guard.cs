5447dfffa624d28e880b1decd0be8e073c1a0cf7
Faraz Masood Khan <faraz@fanaticlab.com>
2012-11-15 04:15:59 +0500

    GunZip compress and decompress help methods in CoreSystem.Util

--- CoreSystem/Util/Guard.cs
+++ CoreSystem/Util/Guard.cs
@@ -7,12 +7,22 @@ namespace CoreSystem.Util
 {
     public static class Guard
     {
+        /// <summary>
+        /// Throws ArgumentNullException for null parameter
+        /// </summary>
+        /// <param name="parameter">Value to check against</param>
+        /// <param name="message">Exception message</param>
         public static void CheckNull(object parameter, string message)
         {
             if (parameter == null)
                 throw new ArgumentNullException(message);
         }
 
+        /// <summary>
+        /// First check for Null and throws ArgumentNullException otherwise throws ArgumentException for empty string
+        /// </summary>
+        /// <param name="parameter">Value to check against</param>
+        /// <param name="message">Exception message</param>
         public static void CheckNullOrEmpty(string parameter, string message)
         {
             Guard.CheckNull(parameter, message);
@@ -21,11 +31,27 @@ namespace CoreSystem.Util
                 throw new ArgumentException(message);
         }
 
+        /// <summary>
+        /// First check for Null and throws ArgumentNullException otherwise throws ArgumentException for white space or empty string
+        /// </summary>
+        /// <param name="parameter">Value to check against</param>
+        /// <param name="message">Exception message</param>
         public static void CheckNullOrTrimEmpty(string parameter, string message)
         {
             Guard.CheckNull(parameter, message);
 
             if (parameter.Trim().Length == 0)
+                throw new ArgumentException(message);
+        }
+
+        /// <summary>
+        /// Check using string.IsNullOrWhiteSpace and throws ArgumentException
+        /// </summary>
+        /// <param name="parameter">Value to check against</param>
+        /// <param name="message">Exception message</param>
+        public static void CheckNullOrWhiteSpace(string parameter, string message)
+        {
+            if (string.IsNullOrWhiteSpace(parameter))
                 throw new ArgumentException(message);
         }
     }
