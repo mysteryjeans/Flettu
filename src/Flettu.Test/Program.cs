9b2dc9bdbef5f87729e1755a358b27a30b4160f6
Faraz M. Khan <mk.faraz@gmail.com>
2019-09-19 23:07:34 +0500

    Typo fix

--- src/Flettu.Test/Program.cs
+++ src/Flettu.Test/Program.cs
@@ -11,8 +11,8 @@ namespace Flettu.Test
             Console.WriteLine("Creating AsyncLock object..");
             using (var asyncLock = new AsyncLock())
             {
-                var checkTask1 = Task.Run(async () => await CheckAsyncLockReentranceAsnyc(asyncLock));
-                var checkTask2 = Task.Run(async () => await CheckAsyncLockReentranceAsnyc(asyncLock));
+                var checkTask1 = Task.Run(async () => await CheckAsyncLockReentranceAsync(asyncLock));
+                var checkTask2 = Task.Run(async () => await CheckAsyncLockReentranceAsync(asyncLock));
        
                 Console.WriteLine("Creating AsyncLock<string> object..");
                 var key1 = "Faraz";
@@ -20,8 +20,8 @@ namespace Flettu.Test
 
                 var asyncValueLock = new AsyncValueLock<string>();
 
-                var checkValueTask1 = Task.Run(async () => await CheckAsyncValueLockReentranceAsnyc(asyncValueLock, key1));
-                var checkValueTask2 = Task.Run(async () => await CheckAsyncValueLockReentranceAsnyc(asyncValueLock, key2));
+                var checkValueTask1 = Task.Run(async () => await CheckAsyncValueLockReentranceAsync(asyncValueLock, key1));
+                var checkValueTask2 = Task.Run(async () => await CheckAsyncValueLockReentranceAsync(asyncValueLock, key2));
                 Task.WaitAll(checkTask1, checkTask2, checkValueTask1, checkValueTask2);
 
                 Console.WriteLine("Press any key to stop..");
@@ -30,28 +30,28 @@ namespace Flettu.Test
             }
         }
 
-        private static async Task CheckAsyncLockReentranceAsnyc(AsyncLock asyncLock, int maxReentrances = 2)
+        private static async Task CheckAsyncLockReentranceAsync(AsyncLock asyncLock, int maxReentrances = 2)
         {
             using (await asyncLock.AcquireAsync())
             {
                 Console.WriteLine($"Lock taken as reentrance: {maxReentrances}.. TaskId: {asyncLock.TaskId}");
 
                 if (maxReentrances > 0)
-                    await CheckAsyncLockReentranceAsnyc(asyncLock, maxReentrances - 1);
+                    await CheckAsyncLockReentranceAsync(asyncLock, maxReentrances - 1);
 
                 Console.WriteLine($"Press any key to release reentrance: {maxReentrances} lock... TaskId: {asyncLock.TaskId}");
                 Console.ReadKey();
             }
         }
 
-        private static async Task CheckAsyncValueLockReentranceAsnyc<T>(AsyncValueLock<T> asyncLock, T value, int maxReentrances = 2)
+        private static async Task CheckAsyncValueLockReentranceAsync<T>(AsyncValueLock<T> asyncLock, T value, int maxReentrances = 2)
         {
             using (await asyncLock.AcquireAsync(value))
             {
                 Console.WriteLine($"Lock taken for value: {value} as reentrance: {maxReentrances}.. TaskId: {asyncLock.GetTaskId(value)}");
 
                 if (maxReentrances > 0)
-                    await CheckAsyncValueLockReentranceAsnyc<T>(asyncLock, value, maxReentrances - 1);
+                    await CheckAsyncValueLockReentranceAsync<T>(asyncLock, value, maxReentrances - 1);
 
                 Console.WriteLine($"Press any key to release lock for value: {value} reentrance: {maxReentrances}.. TaskId: {asyncLock.GetTaskId(value)}");
                 Console.ReadKey();
